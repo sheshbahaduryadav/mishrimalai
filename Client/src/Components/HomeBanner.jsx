dd38c162219bc84f3988d3e050fb6900676ef240
Shesh Bahadur Yadav <sheshbahaduryadav2017@gmail.com>
2023-09-30 18:56:43 +0530

    reupdate home banner

--- Client/src/Components/HomeBanner.jsx
+++ Client/src/Components/HomeBanner.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import  bgImg  from "../assets/1.jpg";
-import { styled } from "styled-components";
-
+import bgImg from "../assets/1.jpg";
+import styled from "styled-components";
 
 export const HomeBanner = () => {
   return (
@@ -11,26 +10,27 @@ export const HomeBanner = () => {
           <h1 className=" text-[25px] lg:text-7xl pb-4 font-semibold leading-loose ">
             We Bake With Passion
           </h1>
-          
+
           <p className="text-[16px] pb-[18px] lg:text-xl font-medium leading-loose">
             Volutpat sit amet leo id efficitur mattis turpis
             <br /> dunec bibendum est proin ligula nisl.
           </p>
-          </div>
-          <div className=" pb-24  pt-5">
-          <button className='bg-[#fda326] lg:h-14 lg:w-40 lg:text-xl h-10 w-28 rounded-full font-bold'>
-            Order Now</button>
-       </div>
-       </div>
+        </div>
+        <div className=" pb-24  pt-5">
+          <button className="bg-[#fda326] lg:h-14 lg:w-40 lg:text-xl h-10 w-28 rounded-full font-bold">
+            Order Now
+          </button>
+        </div>
+      </div>
     </BannerWrapper>
   );
 };
 
 const BannerWrapper = styled.div`
-  .banner-image{
-    background-image:url(${bgImg});
-    background-size:100% 100%;
-    background:cover;
-  background-position: center;
+  .banner-image {
+    background-image: url(${bgImg});
+    background-size: 100% 100%;
+    background: cover;
+    background-position: center;
   }
-`
+`;
