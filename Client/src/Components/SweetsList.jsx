36e6b982b82588debec8f9a0a0e0b37f353cd6f1
Shesh Bahadur Yadav <sheshbahaduryadav2017@gmail.com>
2023-10-12 16:57:12 +0530

    link to shop

--- Client/src/Components/SweetsList.jsx
+++ Client/src/Components/SweetsList.jsx
@@ -3,6 +3,7 @@ import Img1 from "../Icon/category-img1.png";
 import Img2 from "../Icon/category-img2.png";
 import Img3 from "../Icon/category-img3.png";
 import Img4 from "../Icon/category-img4.png";
+import { Link } from "react-router-dom";
 export const SweetsList = () => {
   const data = [
     {
@@ -38,11 +39,13 @@ export const SweetsList = () => {
               className="my-1  w-full md:w-1/2 lg:w-1/4 pb-24 text-center hover:bg-[#fcf9ed]"
               key={index}
             >
-              <div className="flex justify-center">
-                <img className="pt-20 pl-4" src={val.images} alt="" />
-              </div>
-              <h2 className="pt-10 text-4xl font-semibold">{val.title}</h2>
-              <h3 className="text-xl pt-10">{val.description}</h3>
+              <Link to="/shop">
+                <div className="flex justify-center">
+                  <img className="pt-20 pl-4" src={val.images} alt="" />
+                </div>
+                <h2 className="pt-10 text-4xl font-semibold">{val.title}</h2>
+                <h3 className="text-xl pt-10">{val.description}</h3>
+              </Link>
             </div>
           );
         })}
